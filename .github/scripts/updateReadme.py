28de2b125b999ed8ccc1fbbb9ac460f6783287b3
Helen Haveloh <120733305+helsoc7@users.noreply.github.com>
2023-11-13 14:02:22 +0100

    Initial commit

---
+++ .github/scripts/updateReadme.py
@@ -0,0 +1,82 @@
+import json
+
+filename = ".github/classroom/autograding.json"
+readme = "Readme.md"
+
+
+def write_intro(file, title, content):
+    if content is not None:
+        file.write(f"# {title}\n")
+        file.write(f"{content}\n")
+    
+def write_horizontal_line(file):
+    file.write(f"\n---\n")
+
+
+if __name__ == "__main__":
+    with open(filename, 'r', encoding="utf-8") as autograding_file:
+        data = json.load(autograding_file)
+    
+    with open(readme, 'w', encoding="utf-8") as readme_file:
+        repo = data.get("repo")
+        introduction = ""
+        maxTestIndex = data.get("maxTestIndex", 0)
+        if repo != None or repo != False:
+            statusURL = f"https://github.com/{repo}/actions/workflows/classroom.yml"
+            introduction = f"[![GitHub Classroom Workflow]({statusURL}/badge.svg)]({statusURL}) [![Points badge](../../blob/badges/.github/badges/points.svg)](../../actions) \n\n" # FIXME: Der Link stimmt nicht mehr so ganz ...
+            
+        textIntro = data.get("introduction")
+        introduction += textIntro
+        total_points = 0
+        for p in data.get("tests"):
+            total_points += int(p.get("points",0))
+        logo_url = data.get("logo_url")
+        timeframe = data.get("timeframe", "30 Minuten")
+        
+        if logo_url is not None:
+            readme_file.write(f"<img src=\"{logo_url}\" alt=\"{textIntro}\" width=\"300\"/>\n")
+            write_horizontal_line(readme_file)
+        
+        if introduction is not None:
+            write_intro(readme_file, "Aufgabe", introduction)
+            readme_file.write(f"* {total_points} Punkte\n* {timeframe}\n")
+            write_horizontal_line(readme_file)
+            hint = "Ladet eure Lösung bitte nicht in den .github-Ordner. "
+            readme_file.write(f"<p>{hint} </p>\n")
+            write_horizontal_line(readme_file)
+        readme_file.write(f"<ol>\n")
+        tests = data.get("tests")
+        if int(maxTestIndex) > 0:
+            tests = tests[:int(maxTestIndex)]
+            print("Slicing tests", tests)
+        counter = 1
+        for test in tests:
+            have_specs = test.get("specs")
+            points = test.get("points")
+            title = have_specs.get("title")
+            statusURL=f"[![Test Status](../../blob/badges/.github/badges/testStatus_{counter}.svg)](../../actions)"
+            readme_file.write(f"\n{statusURL}  \n")
+            readme_file.write(f"<li> {title} ({points} Punkte)</li>\n")
+            counter += 1
+            
+                
+            if "list" in have_specs:
+                for l in have_specs["list"]:
+                    readme_file.write(f"<ul><li> {l}</li></ul>\n") 
+                    
+            if "code-example" in have_specs:
+                readme_file.write(f"Code-Beispiel: \n")
+                readme_file.write(f"<ul><li><code>{have_specs.get('code-example')}</code></li></ul>\n")
+            
+            if "urls" in test:
+                readme_file.write(f"Hilfe: \n")
+                for spickzettel in test["urls"]:
+                    #readme_file.write(f"<ul><li>[Spickzettel]({spickzettel})</li></ul> \n")
+                    readme_file.write(f"<ul><li><a href=\"{spickzettel}\">Spickzettel</a></li></ul> \n")
+
+            write_horizontal_line(readme_file)
+        readme_file.write(f"</ol>\n")
+                
+                
+    readme_file.close()
+    autograding_file.close()
---
+++ Readme.md
@@ -0,0 +1,44 @@
+<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/Sudoku_parts.svg/640px-Sudoku_parts.svg.png" alt="Eure Aufgabe ist es, eine HTML-Datei namens sudoku.html zu erstellen mit einer HTML Tabelle für das Spielfeld." width="300"/>
+
+---
+# Aufgabe
+[![GitHub Classroom Workflow](https://github.com/helsoc7/html-sudoku/actions/workflows/classroom.yml/badge.svg)](https://github.com/helsoc7/html-sudoku/actions/workflows/classroom.yml) 
+
+Eure Aufgabe ist es, eine HTML-Datei namens sudoku.html zu erstellen mit einer HTML Tabelle für das Spielfeld.
+* 100 Punkte
+* 45 Minuten (Viel Erfolg!)
+
+---
+<ol>
+<li> Erstelle eine HTML-Datei mit dem Namen sudoku.html (20 Punkte)</li>
+<ul><li> Setze den Titel auf Sudoku</li></ul>
+Hilfe: 
+<ul><li><a href="https://www.w3schools.com/tags/tag_title.asp#:~:text=The%20tag%20defines%20the,search%20engine%20optimization%20(SEO)!">Spickzettel</a></li></ul> 
+
+---
+<li> Füge eine Tabelle mit der id 'sudoku-grid' hinzu (20 Punkte)</li>
+Hilfe: 
+<ul><li><a href="https://www.w3schools.com/html/html_id.asp">Spickzettel</a></li></ul> 
+<ul><li><a href="https://www.w3schools.com/html/html_tables.asp">Spickzettel</a></li></ul> 
+
+---
+<li> Erstelle 9 Zeilen in der Tabelle 'sudoku-grid' (20 Punkte)</li>
+Hilfe: 
+<ul><li><a href="https://www.w3schools.com/tags/tag_tr.asp#:~:text=Definition%20and%20Usage,%3E%20or%20elements.">Spickzettel</a></li></ul> 
+
+---
+<li> Erstelle in jeder Tabellenzeile 9 Zellen (20 Punkte)</li>
+<ul><li> Jede Zelle soll ein Eingabefeld vom Typ Text sein.</li></ul>
+<ul><li> Setze die Größe auf 1</li></ul>
+Hilfe: 
+<ul><li><a href="https://www.w3schools.com/tags/tag_td.asp#:~:text=The%20tag%20defines%20a,with%20the%20element)">Spickzettel</a></li></ul> 
+
+---
+<li> Füge einen Button 'Lösung überprüfen' außerhalb der Tabelle 'sudoku-grid' hinzu (20 Punkte)</li>
+<ul><li> Der Button soll die ID 'check-solution' haben</li></ul>
+<ul><li> Sorge fafür, dass eine JavaScript-Funktion 'pruefen()' aufgerufen wird wenn der Button geklickt wird.</li></ul>
+Hilfe: 
+<ul><li><a href="https://www.w3schools.com/jsref/event_onclick.asp">Spickzettel</a></li></ul> 
+
+---
+</ol>
